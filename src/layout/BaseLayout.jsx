65fdfe37f02fa42dbcfd9f4b56ab93c4b3c05f93
MatsRahmat <92767137+MatsRahmat@users.noreply.github.com>
2023-08-31 13:39:11 +0700

    Update BaseLayout.jsx
    minor change

--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -34,7 +34,7 @@ export default function BaseLayout() {
   }, [winWidth]);
   return (
     <>
-      {!canPlay && <CompatibleScreen />}
+{/*       {!canPlay && <CompatibleScreen />} */}
 
       <NavigationBar />
       <div
